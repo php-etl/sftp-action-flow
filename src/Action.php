bb275d2ea7ad2ffaa65eea110c4349887b8b16d2
sebprt <sebastien@kiboko.fr>
2023-04-19 15:26:40 +0200

    Ran php-cs-fixer

--- src/Action.php
+++ src/Action.php
@@ -25,11 +25,11 @@ final class Action implements ActionInterface
     {
         $connection = ssh2_connect($this->host, $this->port);
         if (!$connection) {
-            $this->logger->error("Unable to connect to the server. Please check that you are using the correct host and port");
+            $this->logger->error('Unable to connect to the server. Please check that you are using the correct host and port');
         }
 
         if (!ssh2_auth_password($connection, $this->user, $this->password)) {
-            $this->logger->error("Unable to connect to the server. Please check your login information.");
+            $this->logger->error('Unable to connect to the server. Please check your login information.');
         }
 
         $sftp = ssh2_sftp($connection);
@@ -39,9 +39,9 @@ final class Action implements ActionInterface
         fclose($localFile);
 
         if (!$uploaded) {
-            $this->logger->error("Failed to upload file to the SFTP server.");
+            $this->logger->error('Failed to upload file to the SFTP server.');
         }
 
-        $this->logger->info("The file has been uploaded to the SFTP server.");
+        $this->logger->info('The file has been uploaded to the SFTP server.');
     }
 }
